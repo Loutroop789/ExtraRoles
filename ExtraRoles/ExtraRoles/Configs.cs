0e5d22debb156c23bdce7f78391d057c4fc417ec
Loutroop789 <80158660+Loutroop789@users.noreply.github.com>
2021-04-17 09:12:01 +0800

    Add files via upload

---
+++ ExtraRoles/ExtraRoles/Configs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraRoles
+{
+    public static class Configs
+    {
+        public static bool Enable;
+        public static List<int> d9341itms;
+        public static List<int> scp181items;
+        public static int d9341spawnChance;
+        public static int scp181spawnChance;
+        public static int d9341minplys;
+        public static int scp181minplys;
+        public static int scp181LuckyNumber;
+        public static void Reload()
+        {
+           Enable = Qurre.Plugin.Config.GetBool("Enable");
+           d9341itms = Qurre.Plugin.Config.GetIntList("d9341items");
+            d9341spawnChance = Qurre.Plugin.Config.GetInt("d9341spawnChance", 20);
+            d9341minplys = Qurre.Plugin.Config.GetInt("d9341minplys", 20);
+            scp181spawnChance = Qurre.Plugin.Config.GetInt("scp181spawnChance", 20);
+            scp181minplys = Qurre.Plugin.Config.GetInt("scp181minplys", 20);
+            scp181LuckyNumber = Qurre.Plugin.Config.GetInt("scp181LuckyNumber", 25);
+        }
+    }
+}
